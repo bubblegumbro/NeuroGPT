510aaee6cf541a2090557ad8c26f33be6ee1f757
bubblegumbro <71437178+bubblegumbro@users.noreply.github.com>
2024-04-04 23:55:42 +0530

    Update make.py

--- src/trainer/make.py
+++ src/trainer/make.py
@@ -194,7 +194,7 @@ def make_trainer(
         fp16=fp16,
         max_grad_norm=max_grad_norm,
         gradient_accumulation_steps=4,
-        eval_accumulation_steps=1,
+        eval_accumulation_steps=20,
         deepspeed=deepspeed,
         **kwargs
     )
