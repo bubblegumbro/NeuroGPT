a96cbfb10a4f0e21d18ce8bba69f79985c8a3bf9
Shreyas S <96978606+Zhreyu@users.noreply.github.com>
2024-05-28 17:39:12 +0530

    Update make.py

--- src/trainer/make.py
+++ src/trainer/make.py
@@ -266,7 +266,7 @@ def make_trainer(
         fp16=True,
         max_grad_norm=max_grad_norm,
         gradient_accumulation_steps=gradient_accumulation_steps,  # Added gradient accumulation
-        eval_accumulation_steps=4,
+        eval_accumulation_steps=1,
         deepspeed=deepspeed,
         **kwargs
     )
